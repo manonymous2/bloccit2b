79ff45a7fe89fa0c8c5e61d9cc914b8967fc5878
manonymous2 <esther@manonymous.com>
2013-12-08 13:27:35 -0500

    image uploader post

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :avatar, 
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :avatar
   has_many :posts 
   before_create :set_member
   mount_uploader :avatar, AvatarUploader 
