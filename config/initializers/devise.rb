45a62a2239cb5086130a58e1ae837b9f5f1a44f2
manonymous2 <esther@manonymous.com>
2013-12-14 11:33:12 -0500

    configured app for production emails

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'orrett@dav.is'
+  config.mailer_sender = 'esther@manonymous.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
