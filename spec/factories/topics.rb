90bdfe9739d5d2c2f2cc3d7ed964a7a1ff606b1d
manonymous2 <esther@manonymous.com>
2013-12-23 20:04:03 -0500

    saved factory girl files

--- spec/factories/topics.rb
+++ spec/factories/topics.rb
@@ -1,8 +1,7 @@
+# topic.rb
+
 FactoryGirl.define do
-  factory :post do
-    title "First Post"
-    body "This is the newest post. It needs 20 char to be saved."
-    user
-    topic
+  factory :topic do
+    name "Test Topic"
   end
 end
