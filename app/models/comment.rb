2826c8db33a1f7ceb8666c3c9aa98fa69de0de9c
manonymous2 <esther@manonymous.com>
2013-12-26 20:34:34 -0500

    Ajaxified comment creation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
   attr_accessible :body, :post
-
+  default_scope order('updated_at DESC')
 
   after_create :send_favorite_emails
 
