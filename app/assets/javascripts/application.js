72963e8ca33ec55968b84af600b8fef27e1b86ca
manonymous2 <esther@manonymous.com>
2013-12-26 19:33:36 -0500

    Created show/hide jQuery function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,10 +15,15 @@
 //= require bootstrap
 //= require_tree .
 
-
 $(document).ready(function() {
-  $(".js-add-comment").click(function() {
-    $(".js-new-comment").slideDown();
+  $(".js-show-hide").click(function() {
+    var selector = "." + $(this).attr('data-selector');
+    if ($(selector).is(":visible")) {
+      $(selector).slideUp();
+    }
+    else {
+      $(selector).slideDown();
+    }
     return false;
   });
 });
